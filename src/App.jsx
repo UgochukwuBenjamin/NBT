6eb687bbae2271d39e68d5a5f30baf93252efc82
Ugochukwu Benjamin <chizzyugochukwu896@gmail.com>
2026-06-04 09:20:17 -1100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar.jsx";
 import Footer from "./Components/Footer.jsx";
 
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
 import Courses from "./Pages/courses.jsx";
-import CourseDetails from "./pages/CourseDetails.jsx";
-import Contact from "./pages/Contact.jsx";
-import Enroll from "./pages/Enroll.jsx";
+import CourseDetails from "./Pages/CourseDetails.jsx";
+import Contact from "./Pages/Contact.jsx";
+import Enroll from "./Pages/Enroll.jsx";
 
 export default function App() {
   return (
